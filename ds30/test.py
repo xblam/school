393829830f0ca8307dbc08fe587a59730f676d0e
Ben Lam <benlam@Bens-MacBook-Pro.local>
2024-09-19 22:01:02 -0400

    added homework 2

--- ds30/test.py
+++ ds30/test.py
@@ -1,5 +1,31 @@
-# import foo2
-from foo2 import *
-import cat
+import numpy as np
 
-bark()
+class Circle:
+    radius = 0
+    def print(self):
+        return f'circle: rad: {self.radius}'
+
+class Coord(Circle):
+    x = 1
+    y = 2
+    def print(self):
+        return f'cordcirrcle: radius {self.radius}, x = {self.x}, y = {self.y}'
+
+class SmartCircle(Coord):
+    def magnitude(self):
+        return (self.x**2 + self.y**2)**(1/2)
+c = Circle()
+print(c.print())
+cc = Coord()
+print(cc.print())
+s = SmartCircle()
+
+print(s.magnitude())
+
+# print(super(Coord,cc).print())
+
+c = Circle()
+
+
+g = set()
+g.append(1)
